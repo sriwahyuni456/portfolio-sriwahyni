3fdae4366e1f93ba0fd9028f7c83776f1bf464cc
AiceKerime <fathurahmanilham22@gmail.com>
2022-01-24 11:09:48 +0700

    lagi

--- src/App.js
+++ src/App.js
@@ -2,7 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  
+  return(
+      <div className="navbar">
+          <div className="cointener">
+              <div className="w-3/12"></div>
+              <div className="logo text-center uppercase text-2xl">
+                Heh
+              </div>
+              <div className="w-6/12"></div>
+              <div className="nav">
+
+              </div>
+              <div className="w-3/12"></div>
+              <div className="button">
+
+              </div>
+
+          </div>
+      </div>
+  );
 }
 
 export default App;
